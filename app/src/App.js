7d2ee748ed545ab81f4939838fa847d1dc558624
Halabitimothy <Halabitimoty@yahoo.com>
2022-10-03 17:43:51 +0100

    03/10/2022

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import { Nav } from './Component/Nav/Nav';
 import { Home } from "./Pages/Home/Home"
 import { About } from './Pages/About/About'
 import { Contacts } from './Pages/Contact/Contacts'
+import { Notfound } from './Component/Notfound/Notfound';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element = {<Home/>}/>
         <Route path="/About" element = {<About/>}/>
         <Route path="/Contact/:id" element = {<Contacts/>}/>
+        <Route path='*' element = {<Notfound/>}/>
       </Routes>
     </>
   );
--- app/src/Pages/Contact/Contact.jsx
+++ app/src/Pages/Contact/Contact.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 export function Contact() {
   return (
     <>
+      <Link to="/Contact">Contact</Link>
       <Link to="/Contact/1">First Contact</Link>
       <Link to="/Contact/2">Second Contact</Link>
       <Link to="/Contact/3">Third Contact</Link>
